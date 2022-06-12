4aa503a18cbe148792d15fb37150d24c1cd19dee
Rengel Maricela <maricela.rengel@gmail.com>
2022-06-12 20:43:33 -0300

    Sumo funciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,40 @@
 let cantArticulos = Number( prompt('Ingrese la cantidad de articulos a comprar'))
-let subTotal 
-let total
+let subTotal = 0
+let total = 0
 let articulo 
-const tasa = 2
-const iva = 1.21
-let cuotas = Number( prompt('Ingrese en cuantas cuotas desea pagar'))
+let tasa = 2
+let iva = 1.21
+
 let totalCuota
 
 function sumaArticulos(cantArticulos) {
-    if (cantArticulos > 1) {
+    if (cantArticulos >= 1) {
         for (let i = 1; i <= cantArticulos;i++){
             articulo = Number( prompt(`Ingrese el valor del articulo # ${i}`))
             subTotal += articulo
-            console.log(subTotal)
+/*             console.log(subTotal) */
         }
+        console.log(subTotal)
     }
-    else {
+/*     else {
         console.log(`Ingrese el valor del articulo`)
-        articulo = Number( prompt(`Ingrese el valor del articulo # ${i}`))
+        articulo = Number( prompt(`Ingrese el valor del articulo ${i}`))
         subTotal += articulo
         console.log(subTotal) 
-    }
+    } */
 }
-
+let cuotas = Number( prompt('Ingrese en cuantas cuotas desea pagar'))
 function valorCuotas(cuotas) {
+
     if (cuotas > 1) {
-        total = math.round((subTotal*tasa)*iva)
+        total = (subTotal * tasa) * iva
+        console.log('El monto total a pagar es '+total+'$ y cada cuota es de '+ total / cuotas +'$')
+/*         console.log('El valor de cada cuotas es ' + (total / cuotas) +'$') */
     }
     else {
-        total = math.round(subTotal*iva)
-        console.log('El monto total a pagar es ' + monto * tasa + '$')
+        total = subTotal*iva
+        console.log('El monto total a pagar es ' + total + '$')
     }
-}
-sumaArticulos()
-valorCuotas()
+} 
+sumaArticulos(cantArticulos)
+valorCuotas(cuotas)
