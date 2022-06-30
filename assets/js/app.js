336ad3f54aecc9c5614b741a23aac6687e390740
Rengel Maricela <maricela.rengel@gmail.com>
2022-06-29 23:58:44 -0300

    DOM y Eventos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,46 @@
-let subTotal = 0
+const productsContainer = document.querySelector('#landscapeGaleria')
+const productsCesta = document.querySelector('#cestaContenedora')
+
+BBDD.forEach ((item) => {
+    const div = document.createElement('div')
+    div.classList.add('cardProduct')
+    div.innerHTML = `
+                <div class="cardProduct__image">
+                    <img src=${item.img} alt=${item.alt} class="cardAnimation">
+                </div>
+                <div class="cardProduct__description">
+                    <strong class="cardProduct__description__price">$ ${item.precio}</strong>
+                    <button onclick ="sumarCesta(${item.id})" class="cardProduct__comprar">Comprar</button>
+                </div>
+    `
+    productsContainer.append(div)
+})
+
+const cesta = []
+
+const sumarCesta = (id) => {
+    const producto = BBDD.find((prod) => prod.id ===  id)
+    cesta.push(producto)
+    console.log(cesta)
+    renderCesta()
+}
+
+const renderCesta = () => {
+    cesta.forEach((producto) =>{
+        const div = document.createElement('div')
+        div.classList.add('list-group-item d-flex justify-content-between lh-sm')
+        div.innerHTML = `
+                <div>
+                    <h6 class="my-0">${producto.alt}</h6>
+                </div>
+                <span class="text-muted">$${producto.precio}</span>
+        `
+        productsCesta.append(div)
+    })
+
+}
+
+/* let subTotal = 0
 let total = 0
 let art 
 let tasa = 2
@@ -42,10 +84,14 @@ class articulo {
 }
 
 const productos = [
-    new articulo(1, "Aros luna", 2500),
-    new articulo(2, "Aros sol", 3000),
-    new articulo(3, "Pendiente Landscape", 5000)
+    {id:1, descripción:"Aros Luna", precio:2500 },
+    {id:2, descripción:"Aros Sol", precio:3000 },
+    {id:3, descripción:"Pendiente Landscape", precio:5000 },
+    {id:4, descripción:"Aros Desert", precio:4000 },
+    {id:5, descripción:"Aros Mountain", precio:4000 },
+    {id:4, descripción:"Aros Mar", precio:4000 },
 ]
+
 
 function agregarArticulo () {
      let id = Number(prompt('Ingrese ID del producto'))
@@ -83,7 +129,4 @@ function desicion(comprar) {
 
 let comprar = prompt('Desea comprar un artículo?')
 desicion(comprar)
-
-
-
-
+ */
---
+++ assets/js/stock.js
@@ -0,0 +1,8 @@
+const BBDD = [
+    {id:1, descripcion:"Aros Luna", precio:2500, img: '../assets/img/landscape3.png', alt:"moon & mountain earrings" },
+    {id:2, descripcion:"Aros Sol", precio:3000, img: '../assets/img/landscape2.png', alt:"sun & mountain earrings" },
+    {id:3, descripcion:"Pendiente Landscape", precio:5000, img: '../assets/img/landscape.png', alt:"moon & mountain medal" },
+    {id:4, descripcion:"Aros Desert", precio:4000, img: '../assets/img/landscape6.png', alt:"sun & desert earrings" },
+    {id:5, descripcion:"Aros Mountain", precio:4000, img: '../assets/img/landscape4.png', alt:"mountain & river earrings" },
+    {id:4, descripcion:"Aros Ocean", precio:4000, img: '../assets/img/landscape5.png', alt:"moon & ocean earrings" },
+]
