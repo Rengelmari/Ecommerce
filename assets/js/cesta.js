bf094c645da090a8ac3d01b8364fbe5a02a00eb3
Rengel Maricela <maricela.rengel@gmail.com>
2022-07-31 21:45:03 -0300

    Mejoras

--- assets/js/cesta.js
+++ assets/js/cesta.js
@@ -1,20 +1,21 @@
 const productsCesta = document.querySelector('#cestaContenedora')
 const contadorCesta = document.querySelector('#contadorCesta')
 const totalCesta = document.querySelector('#totalCesta')
+const descuento = document.querySelector('#totalConDesc')
+const finalCesta = document.querySelector('#finalCesta')
+
 
 const codigos = [
-    {id:1, codeDesc:"SALE20", descuento:0.20 },
-    {id:2, codeDesc:"SALE30", descuento:0.30 },
-    {id:3, codeDesc:"SALE40", descuento:0.40 },
-    {id:4, codeDesc:"SALE50", descuento:0.50 }
+    {codeDesc:"SALE20", descuento:0.20},
+    {codeDesc:"SALE30", descuento:0.30},
+    {codeDesc:"SALE40", descuento:0.40},
+    {codeDesc:"SALE50", descuento:0.50}
 ]
 
 const cestaJSon = JSON.parse( localStorage.getItem('cesta') ) || []
 
  const renderCesta = () => {
-
     cestaJSon.forEach ((producto) => {
-
         const li = document.createElement('li')
         li.classList.add(`list-group-item`)
         li.innerHTML = `
@@ -28,76 +29,61 @@ const cestaJSon = JSON.parse( localStorage.getItem('cesta') ) || []
 }
 
 renderCesta()
-/* console.log(cestaJSon) */
-
  const renderContCesta = () => {
     contadorCesta.innerText = cestaJSon.length
 }
+
 renderContCesta()
-/* console.log(contadorCesta) */
-/* const validarCode = () => {
-    let codePromo
-    codigos.forEach ((codigo) => {
-        if codePromo == codigo.codeDesc
-        to
-    })
-} */
 
 const renderTotalCesta = () => {
     let total = 0
     cestaJSon.forEach ((producto) => { 
         total += producto.precio
     })
-
-
     totalCesta.innerText = total
 }
 
 renderTotalCesta()
 
 const btnPromo = document.querySelector(`#btnCodDesc`)
-   
+const code = document.getElementById('#promoCode')
 
-btnPromo.addEventListener('click', () => {
-    console.log(codePromo)
-    let totalConDesc = 0
+btnPromo.addEventListener('click', (e) => { e.preventDefault();
+/*     sessionStorage.getItem ('code', JSON.stringify(code))  */
+    let totalDesc = 0
+    let totalfinal = 0
     codigos.forEach ((codigo) => {
-        if (codePromo === codigo.codeDesc) {
-            totalConDesc = total * codigo.descuento
-            sessionStorage.getItem('promoCode', promo)
+        if (code === codigo.codeDesc) {
+            totalDesc = total * codigo.descuento
+            totalfinal = total - totalDesc 
+            descuento.innerText = totalDesc
+            finalCesta.innerText = totalfinal
+            renderCesta()
+            renderTotalCesta()
             Swal.fire({
             position: 'top-end',
             icon: 'success',
             title: 'Descuento aplicado',
             showConfirmButton: false,
-            timer: 100
+            timer: 1500
             })
         }
         else {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
-                text: 'Something went wrong!',
-                footer: '<a href="">Why do I have this issue?</a>'
+                text: 'Código de descuento no válido!',
+                timer: 1500
+
               })
         }
     })
-    totalCesta.innerText = totalConDesc
-    console.log(totalConDesc)
 })
-console.log(totalCesta)
-/* const calcConDesc = () => {
-    const promo = 0
-    sessionStorage.getItem('promoCode', promo)
-    console.log(promo)
-}
 
-btnPromo.onclick = calcConDesc
-console.log(calcConDesc) */
 
 const btnVaciar = document.getElementById(`btnVaciarCesta`)
 
-btnVaciar.addEventListener('click', () => {
+btnVaciar.addEventListener('click', (e) => { e.preventDefault();
     Swal.fire({
         title: 'Seguro?',
         icon: 'warning',
@@ -109,6 +95,11 @@ btnVaciar.addEventListener('click', () => {
       .then((result) => {
         if (result.isConfirmed) {
             cestaJSon.length = 0
+            productsCesta.remove()
+            localStorage.setItem('cestaJSon', JSON.stringify(cestaJSon))
+            renderCesta()
+            renderContCesta()
+            renderTotalCesta()
           Swal.fire(
             'Eliminado!',
             'Su cesta ha sido vaciada.',
